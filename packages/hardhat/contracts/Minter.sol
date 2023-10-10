36584ff1d834830a37b664452c5e849acd9ad3e0
Jan Turk <jan.turk12@gmail.com>
2023-10-11 00:41:51 +0200

    Add Minter
    Added Minter smart contract to be used for minting new Totems.

---
+++ packages/hardhat/contracts/Minter.sol
@@ -0,0 +1,68 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import "./interfaces/ITotems.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+error MinterIncorrectFee();
+error MinterMintingPaused();
+error MinterWithdrawalFailed();
+
+contract Minter is AccessControl {
+    ITotems public immutable totems;
+
+    uint256 private _fee;
+
+    bool private mintingPaused;
+
+    modifier onlyWhenMintingOperational {
+        if (!mintingPaused) revert MinterMintingPaused();
+        _;
+    }
+
+    event FeeUpdated(uint256 newFee);
+
+    constructor(address totems_, uint256 fee_) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        totems = ITotems(totems_);
+        _fee = fee_;
+        mintingPaused = false;
+
+        emit FeeUpdated(fee_);
+    }
+
+    function fee() public view returns (uint256) {
+        return _fee;
+    }
+
+    function isMintingOperational() public view returns (bool) {
+        return !mintingPaused;
+    }
+
+    function updateFee(uint256 newFee) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _fee = newFee;
+
+        emit FeeUpdated(newFee);
+    }
+
+    function mint() public payable onlyWhenMintingOperational {
+        if (msg.value != _fee) revert MinterIncorrectFee();
+// TODO: Add random assignment of elements
+        totems.craft("infernum", 0, 0, msg.sender);
+    }
+
+    function pauseMinting() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        mintingPaused = true;
+    }
+
+    function resumeMinting() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        mintingPaused = false;
+    }
+
+    function withdrawFees(address recipient) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        (bool success , ) = recipient.call{value: address(this).balance}("");
+
+        if (!success) revert MinterWithdrawalFailed();
+    }
+}

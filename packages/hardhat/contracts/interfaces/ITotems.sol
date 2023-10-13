e1a14f140634a72de5cfcb0644e40c20dcc3e8bc
Jan Turk <jan.turk12@gmail.com>
2023-10-13 11:39:31 +0200

    Add ERC-6454 powered methods to ITotems
    ERC-6454 transferability limiting methods have been added to ITotems, so
    that the Quest smart contract is able to pause the transferability.

--- packages/hardhat/contracts/interfaces/ITotems.sol
+++ packages/hardhat/contracts/interfaces/ITotems.sol
@@ -3,4 +3,12 @@ pragma solidity ^0.8.21;
 
 interface ITotems {
     function craft(string memory element, string memory totemUri, uint8 stage, uint8 tier, address to) external;
+
+    function disableTransferability(uint256 tokenId) external;
+
+    function batchDisableTransferability(uint256[] memory tokenIds) external;
+
+    function enableTransferability(uint256 tokenId) external;
+
+    function batchEnableTransferability(uint256[] memory tokenIds) external;
 }
